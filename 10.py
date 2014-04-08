018bd43cda7c02d37dc963a15dd7004a9571bc05
Eli Williams <eli.t.williams@gmail.com>
2014-04-08 00:37:21 -0500

    added problems 9 and 10

---
+++ 10.py
@@ -0,0 +1,26 @@
+import math
+
+def is_prime(num):
+	i = 2
+	mid = int(math.floor(math.sqrt(num)))
+        if num == 1:
+            pass
+        elif num == 2: 
+            return num
+	else:
+            while num % i != 0:
+                if i >= mid:
+                    return num
+                else:
+                    i += 1
+
+primes = []
+
+for x in range(0,2000000):
+    if type(is_prime(x)) is int:
+        primes.append(is_prime(x))
+
+print sum(primes)
+
+
+
---
+++ 6.py
@@ -0,0 +1,3 @@
+nums = range(1, 101)
+squares = [x**2 for x in nums]
+print (sum(nums)**2-sum(squares))
